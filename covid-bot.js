96be49a642836e3af4dc17468f859f6d396969ab
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-05-17 16:53:21 +0200

    Covid-Bot

---
+++ covid-bot.js
@@ -0,0 +1,14 @@
+const { Client, Collection } = require("discord.js");
+const { loadCommands, loadEvents } = require("./util/loader"),
+  moment = require("moment");
+
+const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+client.config = require("./config");
+["commands", " s"].forEach((x) => (client[x] = new Collection()));
+client.info = require("./assets/info.json");
+
+loadCommands(client);
+loadEvents(client);
+
+client.login(client.config.TOKEN);
+
