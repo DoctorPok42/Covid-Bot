9cd5fa37cd8b73e68d398d0b63f8d0b7b14f84e9
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-05-17 17:00:37 +0200

    Covid-Bot

---
+++ commands/General/avatar.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = (client, message, args) => {
+    let member = message.member;
+    if (args[0]) member = message.guild.member(message.mentions.users.first());
+    let user = member.user;
+    var avatar = new MessageEmbed()
+    .setColor(client.info.cyan)
+    .setTimestamp()
+    .setDescription(`**Avatar de ${user}** \n **[.png](${user.displayAvatarURL({ format: "png", size: 512 })}) | [.jpg](${user.displayAvatarURL({ format: "jpg", size: 512 })}) | [.webp](${user.displayAvatarURL({ format: "webp", size: 512})}) | [.gif](${user.displayAvatarURL({ format: "gif", size: 512})})**`)
+    .setImage(user.displayAvatarURL({dynamic: true}))
+    .setFooter(client.info.footer)
+    message.channel.send(avatar)
+}
+
+module.exports.help = {
+  name: "avatar",
+  aliases: ["avatar"],
+  category: "general",
+  description: "Renvoie l'avatar de soit même ou de la personne mentionner'",
+  usage: "",
+  args: false,
+};
