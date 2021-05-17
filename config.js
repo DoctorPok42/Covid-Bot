1a483d035b8080d29733b37881928cfd2b151232
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-05-17 16:57:51 +0200

    Covid-Bot

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  TOKEN: "YOURTOKEN", // Metez ici le token de votre Bot Discord
+  ACTIVITY: "Stopons la Covid-19 !",
+  TYPE: "COMPETING",
+  STATUS: "online",
+  prefix: "c.",
+};
